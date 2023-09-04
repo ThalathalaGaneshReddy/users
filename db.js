1c91c797bf0ccc6760d6ee2c0aacc049ff0081cc
ThalathalaGaneshReddy <137380847+ThalathalaGaneshReddy@users.noreply.github.com>
2023-09-04 15:20:46 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 let mongoose = require("mongoose")
 // let mongodbURL = process.env.mongodbURL
-mongoose.connect('mongodb+srv://ganeshreddy1435:9908334738@cluster001.f9jddv2.mongodb.net/emp?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://username:password@cluster001.f9jddv2.mongodb.net/emp?retryWrites=true&w=majority')
